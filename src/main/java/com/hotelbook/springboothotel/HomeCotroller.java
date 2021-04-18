d6db34f3fe81d803f1fd983f128ec4d30a80d029
PolaroidBomb <63343034+PolaroidBomb@users.noreply.github.com>
2021-04-18 15:56:12 +0200

    register

--- src/main/java/com/hotelbook/springboothotel/HomeCotroller.java
+++ src/main/java/com/hotelbook/springboothotel/HomeCotroller.java
@@ -1,14 +1,31 @@
 package com.hotelbook.springboothotel;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hotelbook.springboothotel.objects.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
+
 public class HomeCotroller {
+    private static final Logger log = LoggerFactory.getLogger(HomeCotroller.class);
 
     @GetMapping("/")
     public String homePage()
     {
         return "Witaj w hotelu marmurr";
     }
+    @GetMapping("/register" )
 
-}
+
+    public String register(@ModelAttribute User user, Model model) {
+        model.addAttribute("user", user);
+        return "register";
+    }
+    @PostMapping("/register")
+    public void save(User user){
+            log.info(">> user : {}", user.toString());
+        }
+    }
+
