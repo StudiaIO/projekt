ee9b621d16870adeddba4e0c0fbcccd44940ac41
Konrad Bąba <63343034+PolaroidBomb@users.noreply.github.com>
2021-06-10 03:45:49 +0200

    Delete src directory

--- src/main/java/com/hotelbook/springboothotel/UserMongoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.hotelbook.springboothotel;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserMongoRepository extends MongoRepository<user, String>{
-}
